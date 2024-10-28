6f7499a5766f2d77c0f770b0981bc6b57f68172f
Travcort <tarvkoome@gmail.com>
2024-10-28 09:15:50 +0300

    Search functionality for characters

--- frontend/src/data/Data.js
+++ frontend/src/data/Data.js
@@ -2,11 +2,11 @@ import { create } from 'zustand';
 
 const useData = create((set) => ({
     characters: [],
-    getCharacters: async () => {
-        const response = await fetch('/api/character/9');
+    getCharacters: async (id) => {
+        const response = await fetch(`/api/character/${id}`);
         const data = await response.json();
         set({characters: data.message});
-    } 
+    }
 }))
 
 export default useData;
--- frontend/src/pages/Characters.jsx
+++ frontend/src/pages/Characters.jsx
@@ -1,17 +1,33 @@
 import { useEffect } from "react";
 import DetailsCard from "../components/CharactersCard";
 import useData from "../data/Data";
+import { Container, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
 
 const CharactersPage = () => {
 
     const { getCharacters, characters } = useData();
 
     useEffect(() => {
-        getCharacters();
+        getCharacters(1);
     }, []);
 
+    const handleSearch = (e) => {
+        getCharacters(e.target.value)
+    }
+
     return(
-        <DetailsCard id={characters.id} status={characters.status} name={characters.name} species={characters.species} gender={characters.gender} image={characters.image}  />
+        <>
+            <Container mt={5}>
+                <InputGroup>
+                    <InputLeftElement><FaSearch /></InputLeftElement>
+                    <Input type="text"  placeholder="Search a Character [Numbers Only!]" onChange={handleSearch}/>
+                </InputGroup>
+            </Container>
+            <DetailsCard id={characters.id} status={characters.status} name={characters.name}  
+            species={characters.species} gender={characters.gender} 
+            image={characters.image} origin={characters.origin?.name} location={characters.location?.name} />
+        </>
     );
 }
 
