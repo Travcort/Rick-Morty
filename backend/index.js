83830966f7e132c380c9c62c62a21f19ea56977a
Travcort <tarvkoome@gmail.com>
2024-10-30 14:23:33 +0300

    Version 1.0

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,25 @@ import express from 'express';
 const app = express();
 import { PORT } from './utils/config.js';
 
+import path from 'path';
+const __dirname = path.resolve();
+
 // Logging
 import { info } from './utils/logging.js';
 
 // Routing
 import mortyRoutes from './routes/morty.routes.js';
 app.use('/api', mortyRoutes);
+
+// Check for Environment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    // Routing
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     info(`Server running on \x1b[34mhttp://localhost:${PORT}\x1b[0m`);
